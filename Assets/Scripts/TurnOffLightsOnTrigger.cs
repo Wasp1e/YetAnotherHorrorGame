695abc8b01f4be2e2b015f28d8cf46a332772307
Wasp1e <rgertopov@mail.ru>
2025-03-21 23:17:15 +0300

    Триггер выключения света

---
+++ Assets/Scripts/TurnOffLightsOnTrigger.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class TurnOffLightsOnTrigger : MonoBehaviour
+{
+    public AudioSource audiosource;
+    public AudioClip audioclip;
+
+    // Метод, который вызывается при входе другого коллайдера в триггер
+    private void OnTriggerEnter(Collider other)
+    {
+        // Проверяем, если объект, вошедший в триггер, имеет тег "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Выключаем все источники света с тегом "Light"
+            TurnOffLightsWithTag();
+
+            // Отключаем или уничтожаем триггер
+            DisableTrigger();
+        }
+    }
+
+    // Метод для выключения всех источников света с тегом "Light"
+    private void TurnOffLightsWithTag()
+    {
+        // Находим все объекты с тегом "Light"
+        GameObject[] lightObjects = GameObject.FindGameObjectsWithTag("Light");
+        GameObject[] cylinders = GameObject.FindGameObjectsWithTag("Cylinder");
+
+        // Если объекты найдены
+        if (lightObjects.Length > 0)
+        {
+            audiosource.PlayOneShot(audioclip, 0.7f);
+
+            // Проходим по каждому объекту
+            foreach (GameObject lightObject in lightObjects)
+            {
+                AudioSource audioComponent = lightObject.GetComponent<AudioSource>();
+                // Получаем компонент Light
+                Light lightComponent = lightObject.GetComponent<Light>();
+
+                // Если компонент Light существует, выключаем его
+                if (lightComponent != null)
+                {
+                    lightComponent.enabled = false;
+                    Debug.Log("Источник света " + lightObject.name + " выключен.");
+                }
+                if (audioComponent != null)
+                {
+                    audioComponent.enabled = false;
+                }
+            }
+
+            // Отключаем все объекты с тегом "Cylinder"
+            foreach (GameObject cylinder in cylinders)
+            {
+                cylinder.SetActive(false);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Объекты с тегом 'Light' не найдены.");
+        }
+    }
+
+    // Метод для отключения или уничтожения триггера
+    private void DisableTrigger()
+    {
+        // Отключаем объект, на котором находится этот скрипт
+        gameObject.SetActive(false);
+
+        // Или уничтожаем объект (если он больше не нужен в сцене)
+        // Destroy(gameObject);
+    }
+}
